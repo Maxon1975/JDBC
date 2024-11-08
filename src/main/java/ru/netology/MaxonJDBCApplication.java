8321294f6864818a34ab9c64974179634955a910
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2024-11-09 02:31:53 +0300

    Create MaxonJDBCApplication.java

---
+++ src/main/java/ru/netology/MaxonJDBCApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.maxonjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxonJDBCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxonJDBCApplication.class, args);
+    }
+
+}
